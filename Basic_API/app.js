03d96ea367862887a6256206aa98f911c4492744
Bairoflus <seegiochen144@gmail.com>
2025-04-30 22:34:42 -0600

    Installed nodemon, changed items.html name and created delete endpoint

--- Basic_API/app.js
+++ Basic_API/app.js
@@ -33,7 +33,7 @@ app.get('/', function (req, res) {
 // ITEMS ENDPOINTS
 // VIEW
 app.get('/items/register', function (req, res) {
-  fs.readFile('public/html/items.html', 'utf8', (err, html) => {
+  fs.readFile('public/html/registerItems.html', 'utf8', (err, html) => {
     if (err) {
       // Code 500: Internal Server Error
       res.status(500).send('There was an error loading the HTML file, error code: ' + err);
@@ -127,6 +127,83 @@ app.post('/items/check/id', function (req, res) {
   }
   res.status(200).json(item); // Devolvemos el ítem encontrado
 });
+
+// VIEW DELETE ITEM
+app.get('/items/delete', function (req, res) {
+  fs.readFile('public/html/deleteItemsID.html', 'utf8', (err, html) => {
+    if (err) {
+      // Code 500: Internal Server Error
+      res.status(500).send('There was an error loading the HTML file, error code: ' + err);
+      return;
+    }
+
+    console.log("Sending page...");
+    res.send(html);
+    console.log("Page sent.");
+  });
+});
+
+// Eliminar un ítem por su id
+app.post('/items/delete', function (req, res) {
+  const body = req.body;
+  const id = Number(body.id);
+
+  // Buscamos el índice del ítem a eliminar
+  const index = items.findIndex(item => item.id === id);
+
+  if (!index && index !== 0) {
+    // Code 404: Not Found
+    res.status(404).json({ message: `No se encontró un ítem con id='${id}'.` });
+    return;
+  }
+
+  // Si lo encontramos, lo eliminamos
+  items.splice(index, 1);
+  // Code 200: OK
+  res.status(200).json({ message: `Ítem con id='${id}' eliminado correctamente.` });
+});
+
+// VIEW EDIT ITEM
+app.get('/items/edit', function (req, res) {
+  fs.readFile('public/html/editItemsID.html', 'utf8', (err, html) => {
+    if (err) {
+      // Code 500: Internal Server Error
+      res.status(500).send('There was an error loading the HTML file, error code: ' + err);
+      return;
+    }
+
+    console.log("Sending page...");
+    res.send(html);
+    console.log("Page sent.");
+  });
+});
+
+// Actualizar un ítem por su id
+app.post('/items/edit', function (req, res) {
+  const body = req.body;
+  const id = Number(body.id);
+  const name = body.name;
+  const type = body.type;
+  const effect = body.effect;
+
+  // Buscamos el índice del ítem a editar
+  const index = items.findIndex(item => item.id === id);
+
+  if (!index && index !== 0) {
+    // Code 404: Not Found
+    res.status(404).json({ message: `No se encontró un ítem con id='${id}'.` });
+    return;
+  }
+
+  // Si lo encontramos, lo editamos
+  items[index].name = name;
+  items[index].type = type;
+  items[index].effect = effect;
+
+  // Code 200: OK
+  res.status(200).json({ message: `Ítem con id='${id}' editado correctamente.` });
+});
+
 
 // FIN --- ITEMS ENDPOINTS
 
